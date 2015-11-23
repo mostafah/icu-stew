2758013a68a7db329f134ce1e9c705c92e7bd03a
Shervin Afshar <shervin@shervinafshar.name>
2015-11-22 22:54:32 -0800

    Generating exemplar charts for Arabic and Persian.

--- src/main/groovy/Chart.groovy
+++ src/main/groovy/Chart.groovy
@@ -1,5 +1,47 @@
+import com.ibm.icu.text.UnicodeSet
+import com.ibm.icu.util.LocaleData
+import com.ibm.icu.util.ULocale
+import au.com.bytecode.opencsv.CSVWriter;
+
 /**
  * Created by shervinafshar on 11/21/15.
  */
 class Chart {
+
+    public static void generateChart(ULocale u){
+
+        LocaleData ud = new LocaleData().getInstance(u)
+        UnicodeSet usEsStd = ud.getExemplarSet(0, ud.ES_STANDARD)
+        UnicodeSet usEsAux = ud.getExemplarSet(0, ud.ES_AUXILIARY)
+        UnicodeSet usEsPun = ud.getExemplarSet(0, ud.ES_PUNCTUATION)
+
+        FileWriter fw = new FileWriter("Exemplar-Chart-${u.displayLanguage}.csv")
+        CSVWriter writer = new CSVWriter(fw);
+
+        Collection<UnicodeSet> usList = []
+
+        usList.add(usEsStd)
+        usList.add(usEsAux)
+        usList.add(usEsPun)
+
+        String[] header = ["Unicode Name", "Code Point", "Character",
+                           "Is Letter?", "Is Digit?", "Is Diacritic?",
+                           "Is Bidi Control?"]
+
+        writer.writeNext(header)
+
+        usList.each {
+            it.iterator().each {
+
+                UnicodeCharacter uc = new UnicodeCharacter(it)
+                String[] ucValues = [uc.getName(), uc.codePointString, uc.getString(),
+                                     uc.isLetter(), uc.isDigit(), uc.isDiacritic(),
+                                     uc.isBidiControl()]
+                writer.writeNext(ucValues)
+            }
+        }
+
+        writer.close()
+
+    }
 }
--- src/main/groovy/Stew.groovy
+++ src/main/groovy/Stew.groovy
@@ -12,44 +12,17 @@ public class Stew {
 
     public static void main(String[] args){
 
+        true
 
-        List arabWorld = ['EG', 'DZ', 'SA', 'IQ', 'SD', 'MA', \
-                  'YE', 'SY', 'TN', 'JO', 'LB', 'LY', \
-                  'PS', 'AE', 'SO', 'SS', 'MR', 'KW', \
-                  'OM', 'TD', 'QA', 'IL', 'BH', 'EH', \
-                  'KM', 'TR', 'ER', 'ML', 'NG', 'CM', \
-                  'DJ', 'NE', 'KE', 'CY', 'TJ']
+    }
 
-        ULocale u1 = new ULocale('ar')
-        LocaleData ud = new LocaleData()
-        LocaleData ud1 = ud.getInstance(u1)
-        UnicodeSet esStrd1 = ud1.getExemplarSet(0, ud.ES_STANDARD)
+    public static void generateCharts(){
 
-        for (t in arabWorld){
-            ULocale u2 = new ULocale("ar-${t}")
-            LocaleData ud2 = ud.getInstance(u2)
-            UnicodeSet esStrd2 = ud2.getExemplarSet(0, ud.ES_STANDARD)
+        Chart c = new Chart()
 
-            if (!esStrd2.equals(esStrd1)){
-                println t
-            }else{
-                String l = esStrd2.iterator().collect { it }
-                println "${t}: ${l}"
-            }
-        }
+        c.generateChart(new ULocale('ar'))
+        c.generateChart(new ULocale('fa'))
 
-        for (c in esStrd1.iterator()){
-
-            UnicodeCharacter uc = new UnicodeCharacter(c)
-
-            println uc.codePointString
-            println uc.getName()
-            println uc.isLetter()
-            println uc.isDiacritic()
-            println uc.isDigit()
-
-
-        }
 
 
     }
